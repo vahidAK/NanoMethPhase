88e33bd86c7af6d17605151a561c1a77ffdd1a2a
Vahid Akbari <vakbari@bcgsc.ca>
2022-12-18 23:28:46 -0800

    Update DSS_DMA.R

--- nanomethphase/DSS_DMA.R
+++ nanomethphase/DSS_DMA.R
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-library(sys)
+suppressPackageStartupMessages(library("sys"))
 suppressPackageStartupMessages(library("R.utils"))
 suppressPackageStartupMessages(library("DSS"))
 args <- commandArgs(trailingOnly = TRUE)
